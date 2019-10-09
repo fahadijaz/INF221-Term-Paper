957b2b2295ab29cf4af428162d63031d7bd8adf1
Nicolai Munsterhjelm <nicmunst@nmbu.no>
2019-10-09 15:42:16 +0200

    Empty functions for sorting algorithms

---
+++ benchmarking.py
@@ -0,0 +1,13 @@
+def merge_sort(unsorted):
+    """Sorts a list using the 'merge-sort' algorithm."""
+    pass
+
+
+def heap_sort(unsorted):
+    """Sorts a list using the 'heap-sort' algorithm."""
+    pass
+
+
+def quick_sort(unsorted):
+    """Sorts a list using the 'quick-sort' algorithm."""
+    pass
