d05d0e1be2ce135d0078e51c9e69b4e6c35e2f00
Nicolai Munsterhjelm <nicmunst@nmbu.no>
2019-10-10 14:50:34 +0200

    Completed code for merge sort

--- benchmarking.py
+++ benchmarking.py
@@ -25,16 +25,25 @@ def merge(a, first_index, middle_index, last_index):
             a[k] = right[j]
             j += 1
 
+    return a
+
 
 def merge_sort(a, first_index, last_index):
     """Sorts a list using the 'merge-sort' algorithm."""
     if first_index < last_index:
         middle_index = (first_index + last_index) // 2
-        first_sorted = merge_sort(a, first_index, middle_index)
-        last_sorted = merge_sort(a, middle_index + 1, last_index)
-        merge()
+        merge_sort(a, first_index, middle_index)
+        merge_sort(a, middle_index + 1, last_index)
+        return merge(a, first_index, middle_index, last_index)
+
+
+def build_max_heap(a):
+    """Builds a max heap"""
     pass
 
+def max_heapify(a):
+    """Max-heapifies a list"""
+    pass
 
 def heap_sort(a):
     """Sorts a list using the 'heap-sort' algorithm."""
@@ -44,3 +53,8 @@ def heap_sort(a):
 def quick_sort(a):
     """Sorts a list using the 'quick-sort' algorithm."""
     pass
+
+
+if __name__ == '__main__':
+    unsorted = [-2, 3, 4, -4, 2, 0, 1, 5, -5, -3, -1]
+    print(merge_sort(unsorted, 1, len(unsorted)) == sorted(unsorted))
